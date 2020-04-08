db8d9ea05537aa9041db8b9991d9792088489464
mk_rutile <mk1988senna.startline@gmail.com>
2020-04-08 15:24:34 +0900

    Q8へコードを追加しました

--- task.rb
+++ task.rb
@@ -47,11 +47,7 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-  numbers2 = []
-  numbers1.each { |n| numbers2 << n * 10 }
-  p numbers2
-
-  number2 = numbers1.map { |n| n * 10 }
+  numbers2 = numbers1.map { |n| n * 10 }
   p numbers2
 end
 
@@ -68,6 +64,7 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
+  programming_languages.map!(&:capitalize)
   upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
