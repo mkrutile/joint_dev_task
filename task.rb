557a8ef6abdbf2ba21c463d073244f0b0e6973dc
mk_rutile <mk1988senna.startline@gmail.com>
2020-04-09 16:41:37 +0900

    Q14-Q16

--- task.rb
+++ task.rb
@@ -111,7 +111,7 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-  user_data = user_data.merge(update_data)
+  user_data = user_data.merge！(update_data)
   puts user_data
 end
 
@@ -119,7 +119,9 @@ def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-
+  data.each_key do |key|
+    puts "キー: #{key}"
+  end
 end
 
 def q15
@@ -127,7 +129,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+  puts data1.key?(:age)? "OK" : "NG"
+  puts data2.key?(:age)? "OK" : "NG"
 end
 
 def q16
@@ -139,7 +142,9 @@ def q16
   ]
 
   # 以下に回答を記載
-
+  users.map {|user|
+   puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}です。"
+  }
 end
 
 class UserQ17
